79fcf915138d852b4775b985abf73c9464d14a82
jeffersonabreu8591-source <jeffersonabreu8591@gmail.com>
2026-05-18 18:47:51 -0300

    comment

--- scripts/temples.js
+++ scripts/temples.js
@@ -3,7 +3,6 @@ const nav = document.querySelector('nav');
 
 menuButton.addEventListener('click', () => {
     nav.classList.toggle('open');
-
     if (nav.classList.contains('open')) {
         menuButton.textContent = '✖';
     } else {
@@ -14,7 +13,6 @@ menuButton.addEventListener('click', () => {
 // Footer year
 document.querySelector('#currentyear').textContent =
 new Date().getFullYear();
-
-// Last modified
+// Last modified dnnamic app
 document.querySelector('#lastModified').textContent =
 `Last Modified: ${document.lastModified}`;
