498291f61f6bb88a3608bc9825462262f2152c91
jeffersonabreu8591-source <jeffersonabreu8591@gmail.com>
2026-05-30 13:48:58 -0300

    js correction

--- scripts/place.js
+++ scripts/place.js
@@ -1,5 +1,31 @@
+// lasti modified
+document.getElementById("currentyear").textContent =
+new Date().getFullYear();
 
-const currentYear = new Date().getFullYear();
-document.getElementById("currentyear").textContent = currentYear;
-document.getElementById("lastModified").innerHTML =
+document.getElementById("lastModified").textContent =
 `Last Modification: ${document.lastModified}`;
+
+
+// aa
+const temperature = 10;
+const windSpeed = 5;
+
+
+// this function will calculatre wind chill
+function calculateWindChill(temp, speed) {
+    return (13.12 + 0.6215 * temp - 11.37 * speed ** 0.16 + 0.3965 * temp * speed ** 0.16).toFixed(1);
+}
+
+
+// to displey it I need to specify the element
+const windChillElement =
+document.getElementById("windchill");
+
+
+// I needa check condition to 
+if (temperature <= 10 && windSpeed > 4.8) {
+    windChillElement.textContent =
+        `${calculateWindChill(temperature, windSpeed)} °C`;
+} else {
+    windChillElement.textContent = "N/A";
+}
